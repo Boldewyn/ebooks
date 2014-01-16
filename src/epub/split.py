2b86457227566b3bf7d3d9d6d1256a9aa0abb2d0
Manuel Strehl <git@manuel-strehl.de>
2014-01-16 01:57:08 +0100

    update BeautifulSoup and epub generator

--- src/epub/split.py
+++ src/epub/split.py
@@ -2,7 +2,7 @@
 
 import re
 import sys
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 
 _cache = {}
@@ -12,9 +12,8 @@ def fetch(name):
     """"""
     if name not in _cache or "html" not in _cache[name]:
         _cache[name] = _cache.get(name, {})
-        fh = open("%s.html" % name)
-        _cache[name]["html"] = fh.read()
-        fh.close()
+        with open("%s.html" % name) as fh:
+            _cache[name]["html"] = fh.read()
     return _cache[name]["html"]
 
 
@@ -23,21 +22,21 @@ def split(name):
     if name not in _cache or "cont" not in _cache[name]:
         _cache[name] = _cache.get(name, {})
         soup = BeautifulSoup(fetch(name))
-        lang = soup.html["xml:lang"]
-        article = soup.find("article", {"class": "book"})
-        meta = soup.html.head.findAll(["meta", "link"])
+        lang = soup.html["lang"]
+        article = soup.find("article")
+        meta = soup.html.head.find_all(["meta", "link"])
         header = article.find("header")
         sections = []
         toc = None
-        for s in article.findAll("section", recursive=False):
+        for s in article.find_all("section", recursive=False):
             if s.get("id", "") != "Table_of_Contents":
                 sections.append(s)
             else:
                 toc = s
         statics = []
-        for s in article.findAll(src=re.compile(r"^[a-zA-Z0-9_-].*")):
+        for s in article.find_all(src=re.compile(r"^[a-zA-Z0-9_-].*")):
             statics.append(s["src"])
-        for s in article.findAll(href=re.compile(r"^[a-zA-Z0-9_-].*")):
+        for s in article.find_all(href=re.compile(r"^[a-zA-Z0-9_-].*")):
             statics.append(s["href"])
         _cache[name]["cont"] = [header, meta, toc, sections, lang, list(set(statics))]
     return _cache[name]["cont"]
@@ -52,9 +51,12 @@ def get_meta(soup):
                 r[node["name"].split(".")[1]] = node.get("content")
                 continue
         if node.name == "link":
-            if node.get("rel", "").startswith("dc.") and \
-               node["rel"].split(".")[1] not in r:
-                r[node["rel"].split(".")[1]] = node.get("href")
+            rel = node.get("rel", "")
+            if isinstance(rel, list):
+                rel = rel[0]
+            if rel.startswith("dc.") and \
+               rel.split(".")[1] not in r:
+                r[rel.split(".")[1]] = node.get("href")
                 continue
     return r
 
