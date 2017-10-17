43570c2ab035a48a39c802d3e35d020305460a7c
Manuel Strehl <git@manuel-strehl.de>
2017-10-18 00:12:17 +0200

    start serving responses from cache first

--- docs/sw.js
+++ docs/sw.js
@@ -1,2 +1,2 @@
-/*version 0.1.0-a1*/
+/*version 0.1.0-a2*/
 importScripts('static/sw.js');
--- src/js/sw.js
+++ src/js/sw.js
@@ -30,3 +30,35 @@ self.addEventListener('install', function(evt) {
     ]);
   }));
 });
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(
+    update(evt.request)
+    .then(refresh)
+  );
+});
+
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request);
+  });
+}
+
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
+
+
+function refresh(response) {
+  // TODO
+}
